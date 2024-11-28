319bda2bce18087366e339f9216ee4dd6cfb1ae3
Gustavoo-S <Gullsanttos7890@gmail.com>
2024-11-28 01:21:27 -0300

    Feat: adicionando as classes, Controller, Model e Service

---
+++ src/main/java/com/mycompany/apijavaevents/resources/EventoService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.apijavaevents.resources;
+
+/**
+ *
+ * @author gulls
+ */
+public class EventoService {
+    
+}
